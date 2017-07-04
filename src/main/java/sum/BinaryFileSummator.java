2d5b937f1ebbf840eb8de21a9c6309ae1f3431e3
Javaronok <dmitry.gorchakov.murom@gmail.com>
2017-07-04 16:52:17 +0300

    Fork/Join task implementation

--- src/main/java/sum/BinaryFileSummator.java
+++ src/main/java/sum/BinaryFileSummator.java
@@ -8,10 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
 
 /**
  * User: Gorchakov Dmitriy
@@ -40,20 +43,18 @@ public class BinaryFileSummator {
 
     System.out.println("Available processors: " + threads);
 
-    if (chunkSize % ChunkReader.BUFFER_LENGTH != 0)
-      chunkSize = (chunkSize/ChunkReader.BUFFER_LENGTH)*ChunkReader.BUFFER_LENGTH + ChunkReader.BUFFER_LENGTH; 
-
-    ExecutorService executor = Executors.newFixedThreadPool(4);
-    List<Future<Long>> futureResults = new LinkedList<>();
-  
-    for (long pos = 0; pos < size; pos += chunkSize) {
-      futureResults.add(executor.submit(new ChunkReader(file, pos, chunkSize)));
+    if (chunkSize % ChunkReaderTask.BUFFER_LENGTH != 0) {
+      chunkSize = (chunkSize / ChunkReaderTask.BUFFER_LENGTH) * ChunkReaderTask.BUFFER_LENGTH + ChunkReaderTask.BUFFER_LENGTH;
     }
 
-    executor.shutdown();    
+    ForkJoinPool pool = new ForkJoinPool(threads);
+    List<ForkJoinTask<Long>> tasks = new ArrayList<>();
+    for (long pos = 0; pos < size; pos += chunkSize) {
+      tasks.add(pool.submit(new ChunkReaderTask(file, pos, chunkSize)));
+    }
 
-    for (Future<Long> f : futureResults) {
-      accumulator += f.get();
+    for (ForkJoinTask<Long> t : tasks) {
+      accumulator += t.join();
     }
 
     System.out.println(accumulator);
@@ -64,42 +65,46 @@ public class BinaryFileSummator {
     System.out.println("Enter file path");
   }
   
-  static class ChunkReader implements Callable<Long> {
+  static class ChunkReaderTask extends RecursiveTask<Long> {
     private static final int BUFFER_LENGTH = 8192;
     
     final Path file;
     final long seekPosition;
     final long chunkSize;
 
-    public ChunkReader(Path file, long seekPosition, long chunkSize) {
+    public ChunkReaderTask(Path file, long seekPosition, long chunkSize) {
       this.file = file;
       this.seekPosition = seekPosition;
       this.chunkSize = chunkSize;
     }
 
     @Override
-    public Long call() throws Exception {
+    public Long compute() {
       long accumulator = 0;
       long readBytes = 0;
-      try (SeekableByteChannel channel = Files.newByteChannel(file, StandardOpenOption.READ)) {
-        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_LENGTH);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-        channel.position(seekPosition);
-
-        int count = channel.read(buffer);
-        while (readBytes < chunkSize && count != -1) {
-          buffer.rewind();
-
-          int position = 0;
-          while (position < count) {
-            accumulator += buffer.getInt();
-            position = buffer.position();
+      try {
+        try (SeekableByteChannel channel = Files.newByteChannel(file, StandardOpenOption.READ)) {
+          ByteBuffer buffer = ByteBuffer.allocate(BUFFER_LENGTH);
+          buffer.order(ByteOrder.LITTLE_ENDIAN);
+  
+          channel.position(seekPosition);
+  
+          int count = channel.read(buffer);
+          while (readBytes < chunkSize && count != -1) {
+            buffer.rewind();
+  
+            int position = 0;
+            while (position < count) {
+              accumulator += buffer.getInt();
+              position = buffer.position();
+            }
+            buffer.clear();
+            readBytes += count;
+            count = channel.read(buffer);
           }
-          buffer.clear();
-          readBytes += count;
-          count = channel.read(buffer);
         }
+      } catch (IOException e) {
+        throw new RuntimeException("IO error:", e);
       }
       return accumulator;
     }
