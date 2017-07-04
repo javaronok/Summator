a3efaf414c5a362715c562a40a0539e4ca0d7df9
Javaronok <dmitry.gorchakov.murom@gmail.com>
2017-07-04 16:14:23 +0300

    Fork implementation

--- src/main/java/sum/BinaryFileSummator.java
+++ src/main/java/sum/BinaryFileSummator.java
@@ -7,6 +7,9 @@ import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * User: Gorchakov Dmitriy
@@ -14,7 +17,7 @@ import java.nio.file.Paths;
  */
 public class BinaryFileSummator {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
     if (args.length != 1) {
       printUsage();
       System.exit(1);
@@ -28,24 +31,20 @@ public class BinaryFileSummator {
     }
 
     long accumulator = 0;
+    int chunkSize = 16;
 
-    try (SeekableByteChannel channel = Files.newByteChannel(file)) {
-      ByteBuffer buffer = ByteBuffer.allocate(8);
-      buffer.order(ByteOrder.LITTLE_ENDIAN);
+    ExecutorService executor = Executors.newFixedThreadPool(4);
+    List<Future<Long>> futureResults = new LinkedList<>();
 
-      int count = channel.read(buffer);
-      while (count != -1) {
-        buffer.rewind();
-
-        int position = 0;
-        while (position < count) {
-          accumulator += buffer.getInt();
-          position = buffer.position();
-        }
-        buffer.clear();
-        count = channel.read(buffer);
-      }
+    for (long pos = 0; pos < size; pos += chunkSize) {
+      futureResults.add(executor.submit(new ChunkReader(file, pos, chunkSize)));
     }
+
+    for (Future<Long> f : futureResults) {
+      accumulator += f.get();
+    }
+
+    executor.shutdown();
 
     System.out.println(accumulator);
   }
@@ -53,4 +52,38 @@ public class BinaryFileSummator {
   public static void printUsage() {
     System.out.println("Enter file path");
   }
+
+  static class ChunkReader implements Callable<Long> {
+    final Path file;
+    final long seekPosition;
+    final int chunkSize;
+
+    public ChunkReader(Path file, long seekPosition, int chunkSize) {
+      this.file = file;
+      this.seekPosition = seekPosition;
+      this.chunkSize = chunkSize;
+    }
+
+    @Override
+    public Long call() throws Exception {
+      long accumulator = 0;
+      try (SeekableByteChannel channel = Files.newByteChannel(file)) {
+        ByteBuffer buffer = ByteBuffer.allocate(chunkSize);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        channel.position(seekPosition);
+
+        int count = channel.read(buffer);
+
+        buffer.rewind();
+
+        int position = 0;
+        while (position < count) {
+          accumulator += buffer.getInt();
+          position = buffer.position();
+        }
+      }
+      return accumulator;
+    }
+  } 
 }
