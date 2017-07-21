ca63d6423bdaa5c26579dfd3a7335cffc053a57e
Javaronok <dmitry.gorchakov.murom@gmail.com>
2017-07-21 16:44:17 +0300

    Writer refactoring

--- src/main/java/sum/BinaryIntFileWriter.java
+++ src/main/java/sum/BinaryIntFileWriter.java
@@ -12,6 +12,29 @@ import java.util.Date;
  * Date: 02.07.2017.
  */
 public class BinaryIntFileWriter {
+  private final Path file;
+
+  public BinaryIntFileWriter(Path file) {
+    this.file = file;
+  }
+
+  public long write(long packageCount) throws IOException {
+    try (SeekableByteChannel channel = Files.newByteChannel(file, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+      for (int it = 0; it < packageCount; it++) {
+        ByteBuffer buffer = ByteBuffer.allocate(20);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        for (int i = 1; i <= 5; i++) {
+          buffer.putInt(i);
+        }
+        buffer.flip();
+        channel.write(buffer);
+      }
+    }
+
+    return Files.size(file);
+  }
+
   public static void main(String[] args) throws IOException {
     if (args.length != 2) {
       printUsage();
@@ -23,25 +46,12 @@ public class BinaryIntFileWriter {
 
     long time = new Date().getTime();    
 
-    try (SeekableByteChannel channel = Files.newByteChannel(file, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
-      for (int it = 0; it < count; it++) {
-        ByteBuffer buffer = ByteBuffer.allocate(20);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-        for (int i = 1; i <= 5; i++) {
-          buffer.putInt(i);
-        }
-        buffer.flip();
-        channel.write(buffer);
-      }  
-    }
-
-    long size = Files.size(file);
+    long size = new BinaryIntFileWriter(file).write(count);
     System.out.println(size);
     System.out.println("Time: " + (new Date().getTime() - time) + " ms");
   }
 
   public static void printUsage() {
-    System.out.println("Enter file path and count");
+    System.out.println("Enter file path and packages count");
   }  
 }
