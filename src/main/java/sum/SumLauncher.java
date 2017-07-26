bc2b648746035b341f8b325dade337113579eac3
Javaronok <dmitry.gorchakov.murom@gmail.com>
2017-07-26 16:12:19 +0300

    Debug mode

--- src/main/java/sum/SumLauncher.java
+++ src/main/java/sum/SumLauncher.java
@@ -29,22 +29,29 @@ public class SumLauncher {
         int threads = Runtime.getRuntime().availableProcessors();
         int bufferLength = BinaryFileSummator.DEFAULT_BUFFER_LENGTH;
 
+        boolean isDebug = false;
+
         for (int i = 1; i < argsLength; i++) {
             final String arg = args[i];
             if ("-threads".equals(arg) && ++i < argsLength) {
                 threads = Integer.valueOf(args[i]);
             } if ("-bufferLength".equals(arg) && ++i < argsLength) {
                 bufferLength = Integer.valueOf(args[i]);
+            } if ("-debug".equals(arg) && ++i <= argsLength) {
+                isDebug = true;
             }
         }
 
-        System.out.println("Available processors: " + threads);
+        if (isDebug)
+          System.out.println("Available processors: " + threads);
 
         long time = new Date().getTime();
         long result = new BinaryFileSummator(file).calcSum(threads, bufferLength);
 
-        System.out.println("Result: " + result);
-        System.out.println("Execute time: " + (new Date().getTime() - time) + " ms");
+        System.out.println(isDebug ? "Result: " : "" + result);
+
+        if (isDebug)  
+          System.out.println("Execute time: " + (new Date().getTime() - time) + " ms");
     }
 
     public static void printUsage() {
@@ -53,6 +60,7 @@ public class SumLauncher {
                 "<file path> (required, example data/simple.txt)\n" +
                 "-threads <number of processors> (optional)\n" +
                 "-bufferLength <length of buffer> (optional, must be multiple of 4), default: 8192\n" +
+                "-debug debug mode, default: false\n" +
                 "-help\n" +
                 "");
     }
